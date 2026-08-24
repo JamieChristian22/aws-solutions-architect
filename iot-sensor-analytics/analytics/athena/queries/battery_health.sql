7f13003901f027c79aa5c49401ba962c953e3411
Jamie Christian II <jamiechristian557@gmail.com>
2026-08-23 20:39:33 -0400

    Delete iot-sensor-analytics directory

--- iot-sensor-analytics/analytics/athena/queries/battery_health.sql
+++
@@ -1,7 +0,0 @@
-SELECT device_id,
-       min(battery_pct) AS min_battery_pct,
-       avg(battery_pct) AS avg_battery_pct
-FROM iot_telemetry.readings
-WHERE from_iso8601_timestamp(ts) >= current_timestamp - INTERVAL '24' hour
-GROUP BY 1
-ORDER BY min_battery_pct ASC;
--- iot-sensor-analytics/ops/cloudwatch-dashboard-notes.md
+++
@@ -1,5 +0,0 @@
-CloudWatch Dashboard surfaces:
-- Lambda error count & p95 duration
-- Kinesis iterator age (ms)
-- Alert volume over time (SNS publishes)
-Use this as NOC-style live health for the line/factory.
